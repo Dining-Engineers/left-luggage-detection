7a0e3a3c641bf8c2e65f0aaaab12b044ffd7508c
Matteo Bruni <matteo.bruni@gmail.com>
2014-02-24 18:13:24 +0100

    moar mock

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'cv2', 'freenect']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'cv2', 'freenect', 'pygame', 'pykdtree.kdtree']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
