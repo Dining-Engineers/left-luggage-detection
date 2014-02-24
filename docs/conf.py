f4262d003bdd74024c901a006e9a48ecf58e6e17
Matteo Bruni <matteo.bruni@gmail.com>
2014-02-24 18:09:42 +0100

    mock

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath("../"))
 
 import mock
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'cv2', 'freenect']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
